d4d3e98ba7d6ba32d4b80bb3405b1256fff7d461
noothana prasanna <jnoothan998@gmail.com>
2025-06-19 03:14:54 +0530

    port

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def similarity():
     return render_template("UI.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8181, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
